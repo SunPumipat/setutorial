04f8374179ebf4b8e28da3fd8a4bbbd8784c2fb5
SunPumipat <pumipat.tip@gmail.com>
2020-01-14 21:26:32 +0700

    config dimension v2

--- ADT/tutorial2/Dimension.java
+++ ADT/tutorial2/Dimension.java
@@ -8,6 +8,7 @@ public class Dimension {
     public static void main(final String[] args) {
         int[][] array = { { 1, 3, 4 }, { 3, 4, 5 } };
         int[] myAnswer = new int[array.length];
+        int total = 0;
 
         for (int i = 0; i < array.length; i++) {
             myAnswer[i] = summation(array[i]);
@@ -15,7 +16,10 @@ public class Dimension {
 
         for (int i = 0; i < array.length; i++) {
             System.out.println(myAnswer[i]);
+            total = total + myAnswer[i];
         }
+
+        System.out.println("Total: " + total);
 
     }
 
